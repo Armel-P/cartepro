d59a000867835c12a59d2b41c00ff6d10b511ade
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-09-06 18:00:03 +0200

    feat: add all entities

---
+++ src/entities/employee.rs
@@ -0,0 +1,40 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "employee")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    #[sea_orm(column_type = "Float", nullable)]
+    pub balance: Option<f32>,
+    pub qr_token: Option<String>,
+    pub qr_token_created_at: Option<i64>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "crate::entities::user::Entity",
+        from = "Column::Id",
+        to = "crate::entities::user::Column::Id"
+    )]
+    User,
+    #[sea_orm(has_many = "crate::entities::transaction::Entity")]
+    Transaction,
+}
+
+impl Related<crate::entities::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl Related<crate::entities::transaction::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Transaction.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
